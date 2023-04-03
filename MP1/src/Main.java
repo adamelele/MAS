659395dccfb910cbd57db8671ffed48e88a4f4f8
adamelele <adam.sirodzki@gmail.com>
2023-04-03 22:08:15 +0200

    almost done mp1

--- MP1/src/Main.java
+++ MP1/src/Main.java
@@ -1,5 +1,18 @@
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    private void setWorkers(){
+        Worker worker1 = new Worker(new String[]{"Adam", "Michal"} , "Przykladowy", LocalDate.of(2000,2,23), 5820.60);
+        Worker worker2 = new Worker(new String[]{"Kacper"}, "Example", LocalDate.of(1998,8,30), 4910.20, 12);
+    }
+
+
+
 }
--- MP1/src/Worker.java
+++ MP1/src/Worker.java
@@ -1,15 +1,21 @@
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Worker implements Serializable {
 
+    List<Worker> extent = new ArrayList<>();
+
     // Atrybut powtarzalny
-    private String names;
+    private String[] names;
 
     private String surrname;
 
     // Atrybut złożony
-    private LocalDate hireDate;
+    private LocalDate birthDate;
 
     // Atrybut klasowy
     private static final String companyName = "Example Company Corp.";
@@ -18,10 +24,46 @@ public class Worker implements Serializable {
     private int age;
 
     // Atrybut opcjonalny
-    private Double bonus;
+    private double bonusPercentage;
 
+    private double baseSalary;
 
+    public Worker(String[] names, String surrname, LocalDate birthDate, double baseSalary){
+        this.names = names;
+        this.surrname = surrname;
+        this.birthDate = birthDate;
+        this.age = countAge(birthDate);
+        this.baseSalary = baseSalary;
+        extent.add(this);
 
+    }
 
+    public Worker(String[] names, String surrname, LocalDate birthDate, double baseSalary, double bonusPercentage){
+        this.names = names;
+        this.surrname = surrname;
+        this.birthDate = birthDate;
+        this.age = countAge(birthDate);
+        this.bonusPercentage = bonusPercentage;
+        this.baseSalary = baseSalary;
+        extent.add(this);
+    }
 
+    private int countAge(LocalDate birthDate){
+        return Period.between(birthDate, LocalDate.now()).getYears();
+    }
+
+    //przesłonięcie
+    @Override
+    public String toString() {
+        return  "names=" + Arrays.toString(names) +
+                ", surrname='" + surrname + '\'' +
+                ", birthDate=" + birthDate +
+                ", age=" + age +
+                ", salary="+ baseSalary +
+                ", bonusPercentage=" + bonusPercentage;
+    }
+
+    private void showExtension(){
+        System.out.println(extent);
+    }
 }
