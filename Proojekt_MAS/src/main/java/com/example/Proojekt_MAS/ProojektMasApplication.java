862c097e965e8ec8f175206e3bcb957c552858a8
adamelele <adam.sirodzki@gmail.com>
2023-06-20 20:24:19 +0200

    pro init

---
+++ Proojekt_MAS/src/main/java/com/example/Proojekt_MAS/ProojektMasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proojekt_MAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProojektMasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProojektMasApplication.class, args);
+	}
+
+}
---
+++ Proojekt_MAS/src/test/java/com/example/Proojekt_MAS/ProojektMasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proojekt_MAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProojektMasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
